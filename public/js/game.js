e46b5d410fa15c1b169a7ae839fc50a57b504ad3
Jose Minaya <ricardo.vasquez.pe@gmail.com>
2017-07-16 18:41:04 -0500

    finsisss

--- public/js/game.js
+++ public/js/game.js
@@ -1,3 +1,8 @@
 ﻿function startGame(){
 	$('#button-start').find('button').addClass('desaparecer');
+	$('#game').addClass('aparecer');
+}
+function finishGame(){
+	$('#contenido').css("display","none");
+	$('#finish').css("display","block");
 }
