a7356a8c9a823e09d47b1d3e59de085d1ff3dca0
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2017-07-16 20:28:44 -0500

    Adding  dice

--- public/js/game.js
+++ public/js/game.js
@@ -6,3 +6,11 @@ function finishGame(){
 	$('#contenido').css("display","none");
 	$('#finish').css("display","block");
 }
+
+var rollTheDice = function() {
+    var output = '',
+    	faceValue = Math.floor(Math.random() * 6);
+        output += "&#x268" + faceValue + "; ";
+    
+    document.getElementById('dice').innerHTML = output;
+}
